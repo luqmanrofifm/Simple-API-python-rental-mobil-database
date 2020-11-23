db90fa8dbec399136ba5f54088d047ebc39279c5
luqmanrofifm <luqmanrmrofif@yahoo.co.id>
2020-11-23 22:56:58 +0700

    Firsr=t commit

---
+++ API.py
@@ -0,0 +1,58 @@
+import flask
+from flask import request, jsonify
+from flask import Flask, render_template, request, url_for, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MySQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'luqmanklaten060800'
+app.config['MYSQL_DB'] = 'rental_mobil'
+app.config["DEBUG"] = True
+mysql = MySQL(app)
+# Create some test data for our catalog in the form of a list of dictionaries.
+'''
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+'''
+
+#test first commit
+
+@app.route('/', methods=['GET'])
+def home():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM daftar_mobil")
+    row_headers=[x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data=[]
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+    return jsonify(json_data)
+    #cur.close()
+    #return render_template('home.html', computers=rv)
+    #return str(rv)
+#    return '''<h1>Distant Reading Archive</h1>
+#<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+#@app.route('/api/v1/resources/books/all', methods=['GET'])
+#def api_all():
+#    return jsonify(books)
+
+app.run()
